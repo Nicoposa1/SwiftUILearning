275a6085698022c83b7a2459096975ed394157ef
Nicolas Posa <nicoposa57@gmail.com>
2024-02-17 20:04:47 -0300

    dividers

---
+++ Dividers.swift
@@ -0,0 +1,32 @@
+//
+//  Dividers.swift
+//  curso
+//
+//  Created by Nicolas Posa on 17/02/2024.
+//
+
+import SwiftUI
+
+struct Dividers: View {
+    var body: some View {
+        VStack {
+            Circle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100, alignment: .center)
+            Text("Circulo negro")
+            
+            
+            Divider().frame(height: 8).background(Color.red)
+            
+            
+            Rectangle()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .frame(
+                width: 200, height: 100, alignment: .center)
+            Text("Rectangulo azul")
+            
+        }
+    }
+}
+
+#Preview {
+    Dividers()
+}
