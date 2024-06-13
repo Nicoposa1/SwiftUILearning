4dbbee01d0c1ed1955f7e0dcd81f873e17b6d573
Nicolas Posa <nicoposa57@gmail.com>
2024-06-13 00:53:56 -0300

    practice

---
+++ ZstackPadding.swift
@@ -0,0 +1,25 @@
+//
+//  ZstackPadding.swift
+//  curso
+//
+//  Created by Nicolas Posa on 13/06/2024.
+//
+
+import SwiftUI
+
+struct ZstackPadding: View {
+    var body: some View {
+        
+        
+        ZStack {
+            Color.yellow
+            Text("Platzi")
+        }.ignoresSafeArea()
+        
+        
+    }
+}
+
+#Preview {
+    ZstackPadding()
+}
