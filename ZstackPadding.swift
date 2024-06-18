b71274290d01d3e53cd7a61e45bc00433a5fff14
Nicolas Posa <nicoposa57@gmail.com>
2024-06-17 23:05:15 -0300

    zstack

--- ZstackPadding.swift
+++ ZstackPadding.swift
@@ -13,7 +13,24 @@ struct ZstackPadding: View {
         
         ZStack {
             Color.yellow
-            Text("Platzi")
+            
+            VStack{
+                Image("photo").resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 150, alignment: .center)
+                Text("Esto es una prueba")
+                VStack(alignment: .trailing, spacing: 0){
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                    HStack(alignment: .top){
+                        Text("A").frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Text("B")
+                        Text("S")
+                    }.background(Color.red)
+
+                }.background(Color.blue)
+            }
         }.ignoresSafeArea()
     }
 }
